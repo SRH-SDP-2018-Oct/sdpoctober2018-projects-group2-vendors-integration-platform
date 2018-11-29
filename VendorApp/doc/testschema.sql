4a8666f5e365b4e97d32b40b1fc2f8c0c083539a
Vivek <Vivek@172.21.150.92>
2018-11-29 22:27:55 +0100

    Reason: Addition of 'user_type' table and its association with 'user_master'.
    Association:
    user_master.userType = user_type.id

--- VendorApp/doc/testschema.sql
+++ VendorApp/doc/testschema.sql
@@ -1,40 +1,63 @@
 
-CREATE DATABASE IF NOT EXISTS*/vendor_app /*!40100 DEFAULT CHARACTER SET latin1 */;
+DROP DATABASE vendor_app;
+
+CREATE DATABASE vendor_app;
 
 USE vendor_app;
 
-/*Table structure for table user_master */
+
+DROP TABLE IF EXISTS user_type;
+
+CREATE TABLE user_type (
+   id TINYINT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'User type id. It will be used as referenece in the users table',
+   typeName VARCHAR(20) NOT NULL,
+   deleteFlag TINYINT(1) NOT NULL DEFAULT 0
+) ENGINE=INNODB DEFAULT CHARSET=latin1 ;
+
+INSERT INTO user_type(typeName) VALUES ('admin');
+INSERT INTO user_type(typeName) VALUES ('system');
+INSERT INTO user_type(typeName) VALUES ('normal');
+
+SELECT * FROM user_type;
 
 DROP TABLE IF EXISTS user_master;
 
 CREATE TABLE user_master (
-  userId int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Auto generated userid of the system user to perform management/maintenance',
-  username varchar(75) NOT NULL COMMENT 'user name to login within the application as the system user',
-  uPwd text NOT NULL,
-  uType varchar(15) NOT NULL COMMENT 'User type to segregate whether the user has normal, admin or sub-admin privileges',
-  firstName varchar(30) NOT NULL,
-  lastName varchar(30) NOT NULL,
-  phone varchar(15) DEFAULT '',
-  email varchar(75) DEFAULT '',
-  address varchar(100) DEFAULT '',
-  pin varchar(10) DEFAULT '',
-  country varchar(30) DEFAULT '',
-  deleteFlag tinyint(1) NOT NULL DEFAULT '0' COMMENT 'flag to mark the entry as deleted -> 0-Active, 1-Deleted',
-  createdBy int(11) unsigned NOT NULL COMMENT 'user who registered the given user',
-  createdOn datetime NOT NULL COMMENT 'user created on',
-  modifiedBy int(11) unsigned NOT NULL COMMENT 'user who modified the given user',
-  modifiedOn datetime NOT NULL COMMENT 'user modified on',
-  PRIMARY KEY (userId),
-  UNIQUE KEY username (username)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  userId INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Auto generated userid of the system user to perform management/maintenance',
+  username VARCHAR(75) NOT NULL UNIQUE COMMENT 'user name to login within the application as the system user',
+  uPwd TEXT NOT NULL DEFAULT '' COMMENT 'User Password',
+  userType TINYINT UNSIGNED NOT NULL COMMENT 'User type to define privileges',
+  firstName VARCHAR(30) NOT NULL,
+  lastName VARCHAR(30) NOT NULL,
+  phone VARCHAR(15) DEFAULT '',
+  email VARCHAR(75) DEFAULT '',
+  address VARCHAR(100) DEFAULT '',
+  pin VARCHAR(10) DEFAULT '',
+  country VARCHAR(30) DEFAULT '',
+  deleteFlag TINYINT(1) NOT NULL DEFAULT 0 COMMENT 'flag to mark the entry as deleted -> 0-Active, 1-Deleted' ,
+  createdBy INT UNSIGNED NOT NULL COMMENT 'user who registered the given user',
+  createdOn DATETIME NOT NULL COMMENT 'user created on',
+  modifiedBy INT UNSIGNED NOT NULL COMMENT 'user who modified the given user',
+  modifiedOn DATETIME NOT NULL COMMENT 'user modified on',
+  FOREIGN KEY (userType) REFERENCES user_type(id) ON UPDATE CASCADE ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARSET=latin1 ;
 
 DESC user_master;
 
 
+
 /*Data for the table user_master */
 
-insert  into user_master(userId,username,uPwd,uType,firstName,lastName,phone,email,address,pin,country,deleteFlag,createdBy,createdOn,modifiedBy,modifiedOn) values 
-(1,'system','','system','system','system','','','','','',0,1,'2018-11-28 18:32:08',1,'2018-11-28 18:32:08');
+-- Entry -> ROOT User
+INSERT INTO user_master (userType, createdBy, modifiedBy, username, firstName, lastName, createdOn, modifiedOn)
+SELECT user_type.id, '1', '1', 'root', 'root', 'root', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+FROM user_type
+WHERE user_type.typeName = 'admin';
 
+-- Entry -> SYSTEM User
+INSERT INTO user_master (userType, createdBy, modifiedBy, username, firstName, lastName, createdOn, modifiedOn)
+SELECT user_type.id , um.userId, um.userId, 'system', 'system', 'system', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+FROM user_type, user_master um
+WHERE user_type.typeName = 'system' AND um.userName='root';
 
 SELECT * FROM user_master;
