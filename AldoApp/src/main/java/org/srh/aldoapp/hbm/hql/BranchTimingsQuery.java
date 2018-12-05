4d48350df7ddfeb33d831a4e01f125f065617afb
YASHAS <YASHAS@172.19.171.209>
2018-12-05 19:04:51 +0100

    Query for BranchTimings

---
+++ AldoApp/src/main/java/org/srh/aldoapp/hbm/hql/BranchTimingsQuery.java
@@ -0,0 +1,17 @@
+package org.srh.aldoapp.hbm.hql;
+
+
+	public final class BranchTimingsQuery {
+		public BranchTimingsQuery() {}
+		
+		public static final String FIND_BRANCH_BY_BRANCHID_$P1 = "branchId";
+		public static final String FIND_BRANCH_BY_BRANCHID_$N = "FIND_BRANCH_BY_BRANCHID";
+		public static final String FIND_BRANCH_BY_BRANCHID_$Q = "FROM BranchTimings "
+				+ " WHERE deleteFlag=0 AND branchId=:"+FIND_BRANCH_BY_BRANCHID_$P1;
+
+		public static final String FIND_BRANCH_BY_DAYINWEEK_$P1 = "dayInWeek";
+		public static final String FIND_BRANCH_BY_DAYINWEEK_$N = "FIND_BRANCH_BY_DAYINWEEK";
+		public static final String FIND_BRANCH_BY_DAYINWEEK_$Q = "FROM BranchTimings "
+				+ " WHERE firstName LIKE :"+FIND_BRANCH_BY_DAYINWEEK_$P1;
+	}
+
