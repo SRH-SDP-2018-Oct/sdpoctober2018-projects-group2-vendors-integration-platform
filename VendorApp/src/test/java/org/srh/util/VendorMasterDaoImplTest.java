1ba541440d29662fe0c3a96a54943b684dec95c2
Anglita <Anglitaalvares.anglita@gmail.com>
2018-12-08 14:56:21 +0100

    [Added updated UTCS for Vendor Dao Implementation]

--- VendorApp/src/test/java/org/srh/util/VendorMasterDaoImplTest.java
+++ VendorApp/src/test/java/org/srh/util/VendorMasterDaoImplTest.java
@@ -1,12 +1,12 @@
 package org.srh.util;
 
 import static org.junit.Assert.*;
-
 import org.json.JSONObject;
-
-
 import org.junit.Test;
 import org.srh.vipapp.hbm.dao.impl.VendorMasterDaoImpl;
+import org.srh.vipapp.hbm.dto.VendorMaster;
+
+
 
 public class VendorMasterDaoImplTest {
 
@@ -14,42 +14,72 @@ public class VendorMasterDaoImplTest {
 	public void testFindByIdIntPositive() {
 		JSONObject jsonObject = null;
 		Object obj1 = new VendorMasterDaoImpl().findById(1);
- 		jsonObject	 = new JSONObject(obj1);
- 		int vendorNameValue = jsonObject.getInt("vendorId");
+		jsonObject	 = new JSONObject(obj1);
+		int vendorNameValue = jsonObject.getInt("vendorId");
 		if(vendorNameValue == 1)
 		{
 			assertTrue(true);
 		}
-		
+
 	}
 
+	
 	@Test
-	public void testFindByIdIntNullVendorid() {
-		
-		Object obj = new VendorMasterDaoImpl().findById(0);
-		if(obj == null)
-		{
-			assertTrue(true);
+	public void testFindById() {
+		int id = 1;
+		VendorMaster vendorMaster = new VendorMasterDaoImpl().findById(id);
+		if(vendorMaster != null){
+			assertEquals(id, vendorMaster.getVendorId());
 		}
-		
+		else {
+			// Entry not present in database
+			assertEquals(null, vendorMaster);
+		}
+
 	}
 
 	@Test
 	public void testGetAllVendors() {
 		JSONObject jsonObject = null;
 		Object obj1 = new VendorMasterDaoImpl().getAllVendors();
- 	
- 		
+		jsonObject	 = new JSONObject(obj1);
+		int length = jsonObject.length();
+		if(length != 0)
+		{
+			assertTrue(true);
+		}
+
 	}
 
-	@Test
+/*	@Test
 	public void testFindByVendorNameStringSession() {
 		fail("Not yet implemented");
 	}
-
+*/
 	@Test
-	public void testFindByVendorNameString() {
-		fail("Not yet implemented");
+	public void testFindByVendorNameStringPositive() {
+		String vendorName = "Aldo";
+		VendorMaster vendorMaster = new VendorMasterDaoImpl().findByVendorName(vendorName);
+		if(vendorMaster != null)
+		{
+			assertEquals(vendorName,vendorMaster.getVendorName());
+			
+		}
+		
+
+	}
+
+	@SuppressWarnings("null")
+	@Test
+	public void testFindByVendorNameStringNegative() {
+		String vendorName = "Aldi";
+		VendorMaster vendorMaster = new  VendorMasterDaoImpl().findByVendorName(vendorName);
+		if(vendorMaster == null)
+		{
+			 assertEquals(null,vendorMaster.getVendorName());
+			
+		}
+
 	}
 
 }
