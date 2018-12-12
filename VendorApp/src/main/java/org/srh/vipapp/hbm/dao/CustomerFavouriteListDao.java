8d93226209b66daefe8b7183b3fa894a7132627d
Vivek <Vivek@172.19.165.208>
2018-12-12 12:32:32 +0100

    Commit

--- VendorApp/src/main/java/org/srh/vipapp/hbm/dao/CustomerFavouriteListDao.java
+++ VendorApp/src/main/java/org/srh/vipapp/hbm/dao/CustomerFavouriteListDao.java
@@ -1,6 +1,9 @@
 package org.srh.vipapp.hbm.dao;
 
+import java.util.List;
+
 import org.srh.vipapp.hbm.dto.CustomerFavouriteList;
+import org.srh.vipapp.hbm.dto.ProductsMaster;
 
 /**
  * HBM DAO to perform task related to {@link CustomerFavouriteList} entity.  <br/>
@@ -13,14 +16,20 @@ public interface CustomerFavouriteListDao {
 	 * @param id {@link Long}
 	 * @return customerFavouriteList {@link CustomerFavouriteList}
 	 */
-	public CustomerFavouriteList findById(long id);
+	CustomerFavouriteList findById(long id);
 
 	/**
 	 * Gets the {@link CustomerFavouriteList} entity with the given 'customerId'.
 	 * @param customerId {@link Long}
 	 * @return customerFavouriteList {@link CustomerFavouriteList}
 	 */
-	public CustomerFavouriteList findByCustomerId(long customerId);
+	CustomerFavouriteList findByCustomerId(long customerId);
 
 
+	/**
+	 * Gets all the products from the latest Favourite list.
+	 * @param customerId {@link String}
+	 * @return listProductsMaster {@link List<ProductsMaster>}
+	 */
+	List<ProductsMaster> getFavouriteProducts(long customerId);
 }
